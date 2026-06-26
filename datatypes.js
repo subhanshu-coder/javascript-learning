b4b3c5c083885e9a4f4f5cbb59ecc76f2234c065
subhanshu-pal <subhanshupal7@gmail.com>
2026-06-26 10:44:09 +0530

    Initial commit

---
+++ datatypes.js
@@ -0,0 +1,36 @@
+/* two types of data types in javascript 
+primitive data types and non-primitive data types
+big int, number, string, boolean, null, undefined, symbol
+non-primitive data types are object, array, function
+*/
+
+/*let marks = 90;
+   marks = "hello";
+   marks = true;
+console.log(marks);
+*/
+
+/*
+let marks;
+console.log(marks); // undefined
+*/
+
+/*let marks = null;
+console.log(typeof(marks));*?
+
+// null is an object but it is a primitive data type
+*/
+
+/* 
+let marks ="100";
+let firstName = "rahul";
+let lastName = "sharma";
+let fullName = firstName + " " + lastName;
+let oldMan = `hello ${fullName}`;
+// console.log(fullName);
+console.log(oldMan);
+ */
+
+ let x = symbol();
+ let y = symbol ();
+ console.log(x);
---
+++ index.js
@@ -0,0 +1,43 @@
+// console.log("namshte dunia");
+
+// using var 
+
+// var age =20;
+// if(true){
+//     console.log(age);
+// }
+
+// this is going to be right you defined once but you can reclared again 
+// let x =10;
+// x =20;
+
+// let x =10;
+// let x =20; // this is going to be wrong because you can not redeclare the variable
+
+// this is going to be right because you can declare the variable in different block scope}
+// let x=10;
+
+// {
+// let x =20;
+// }
+
+// {
+//     let
+//     x=10;
+//     console.log(x);
+// }
+//  // 
+
+// const x =10;
+//    const x =20; // this is going to be wrong because you can not redeclare the variable
+//    x = 30; // this is going to be wrong because you can not reassign the variable
+   //    not declare again going to be problem 
+  
+// var we redefine or redeclare the variable but we can not reassign the variable
+// let we can not redeclare the variable but we can reassign the variable
+// const we can not redeclare the variable and we can not reassign the variable
+
+
+
+
+
