937005c6d98cc067807ddc136da64d31d7a844c9
subhanshu-pal <subhanshupal7@gmail.com>
2026-06-27 22:29:28 +0530

    update array.js

---
+++ array.js
@@ -0,0 +1,8 @@
+/* declare an array named 'cities' containing 
+the names of 4 cities as strings. Then,
+london, pairis, new york, tokyo'. and access
+    the third element of the array and store it in a variable named 'favoritecity' or print it to the console.
+*/
+let cities = ['london', 'paris', 'new york', 'tokyo'];
+let favoriteCity = cities[2];
+console.log(cities[2]);
