30e7e367612641acacacc60a7a0bcb3d302c75d1
subhanshu-pal <subhanshupal7@gmail.com>
2026-07-04 22:59:26 +0530

    update function.js

---
+++ function.js
@@ -0,0 +1,12 @@
+// function defitions
+// Functions are reusable code blocks designed for particular tasks
+// You call a function by adding parentheses to its name: name()
+
+// () is carrying parameter
+function greet (){
+     console.log("Hello");
+}
+
+// without function call it will not execute or not print anything
+greet(); // function call
+
