373f9922516ce35a78e8e925386f08806b92b251
subhanshu-pal <subhanshupal7@gmail.com>
2026-07-04 23:38:46 +0530

    update function.js

--- function.js
+++ function.js
@@ -19,9 +19,17 @@ pass(); // function call
 
 // parameter function or argument
 // a,b is parameter and 10,20 is arguments
- 
+
 function sum (a,b){
     console.log("this is the sum of a and b:", a+b);
 
 }
 sum(10,20); // function call with arguments declare
+
+
+// return function
+function add (x,y){
+    return x+y;
+}
+let result = add(2,5);
+console.log ("this is result" + " " + result);
