a9dc628503fc757acc43b6d0a62013668e816ede
subhanshu-pal <subhanshupal7@gmail.com>
2026-07-18 00:16:16 +0530

    update contructor.js

--- contructor.js
+++ contructor.js
@@ -4,3 +4,15 @@
 // To create an object type we use an object constructor function.
 
 // It is considered good practice to name constructor functions with an upper-case first letter.
+
+// In the constructor function, this has no value.
+function person(first, last, age, eye) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyecolor = eye;
+}
+// The value of this will become the new object when a new object is created.
+
+const myFather = new person("john", "doe", 15, "blue");
+console.log("my father name is " + myFather.firstName + " , his age is " + myFather.age + " and his eye color is " + myFather.eyecolor);
