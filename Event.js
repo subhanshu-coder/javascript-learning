38e40ca66705a4b5e9b9ce76ac990aa466c8d056
subhanshu-pal <subhanshupal7@gmail.com>
2026-07-28 13:03:54 +0530

    update all

--- Event.js
+++ Event.js
@@ -1,7 +1,22 @@
-function changetext () {
-    let fpara = document.querySelector(".content");
-    fpara.textContent = "hello i am changed"
-}
-    let fpara = document.querySelector(".content");
+// function changetext () {
+//     let fpara = document.querySelector(".content");
+//     fpara.textContent = "hello i am changed"
+// }
+//     let fpara = document.querySelector(".content");
 
-fpara.addEventListener("click", changetext);
+// fpara.addEventListener("click", changetext);
+
+// ques 1
+let modeBtn = document.querySelector("#mode");
+let currmode = "light"; //dark 
+
+modeBtn.addEventListener("click", () => {
+    if (currmode === "light") {
+        currmode = "dark";
+       document.body.style.backgroundColor = "black";
+    } else {
+        currmode = "light";
+        document.body.style.backgroundColor = "white";
+    }
+    console.log(currmode);
+})
