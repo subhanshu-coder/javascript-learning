defd248bb259db823a08bbc4310664e77723280c
subhanshu-pal <subhanshupal7@gmail.com>
2026-07-30 23:40:03 +0530

    update promise.js

--- promise.js
+++ promise.js
@@ -1,9 +1,20 @@
 const getPromise = () => {
     return new Promise((resolve, reject) => {
         console.log("hii i am good");
-        resolve("success");
+        // resolve("success");
+        reject("error");
     });
 };
+
+let promise = getPromise (); //calling the function or saving in promise variable
+promise.then(() => {  //then block will be executed when promise is resolved
+    console.log("i am in then block");
+});
+
+promise.catch(() => {
+    console.log("i am in catch block");
+})
+
 
 
 
