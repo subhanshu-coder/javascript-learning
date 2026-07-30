bf3a33d010b3899ac22d09189c1da8f442b87d7a
subhanshu-pal <subhanshupal7@gmail.com>
2026-07-30 23:28:43 +0530

    update promise.js

--- promise.js
+++ promise.js
@@ -1,18 +1,22 @@
-// let promise = new Promise((resolve, reject) => {
-//     console.log("hii i am good");
-//     resolve()
-// });
-
-function getdata(dataId, getNextData){
-    return new Promise ((resolve , reject) => {
-        setTimeout(() => {
-            console.log(`data is fetched for ${dataId}`);
-            resolve("success");
-            if (getNextData){
-                getNextData();
-
-            }
-
-        }, 5000);
+const getPromise = () => {
+    return new Promise((resolve, reject) => {
+        console.log("hii i am good");
+        resolve("success");
     });
-}
+};
+
+
+
+// function getdata(dataId, getNextData){
+//     return new Promise ((resolve , reject) => {
+//         setTimeout(() => {
+//             console.log(`data is fetched for ${dataId}`);
+//             resolve("success");
+//             if (getNextData){
+//                 getNextData();
+
+//             }
+
+//         }, 5000);
+//     });
+// }
